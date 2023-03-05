20c854645683af3a6412ddbdb4841f277f17c65b
RaY8118 <parthghadi43@gmail.com>
2023-03-05 16:35:45 +0530

    candidate info

--- api/info.php
+++ api/info.php
@@ -1,15 +1,15 @@
 <?php
     include("connection.php");
 
-    $Name = $_POST['name'];
+    $name = $_POST['name'];
     $uid = $_POST['uid'];
     $info = $_POST['info'];
-    $imag = $_FILES['imag']['name'];
-    $tmpname = $_FILES['imag']['tmpname'];
+    $pic = $_FILES['pic']['name'];
+    $tmp_name = $_FILES['pic']['tmp_name'];
 
 
-    move_uploaded_file($tmpname,"../uploads/$imag");
-    $insert = mysqli_query($connect,"insert into info (name, uid, info, pic) values('$Name','$uid' ,'$info', '$imag' )");
+    move_uploaded_file($tmp_name,"../uploads/$pic");
+    $insert = mysqli_query($connect,"insert into info (name, uid, info, pic) values('$name','$uid' ,'$info', '$pic' )");
     if($insert){
         echo '<script>
                 alert("Infomartion Updated Successfully");
